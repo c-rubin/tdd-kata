ad199bb5741d38f818b3b58fede9cebc4f9844f3
Rubin <c-rubin@outlook.com>
2024-04-18 22:14:55 +0200

    api exists test - creation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Step 1
 
 firstly, I created a test class, and just made sure it worked correctly
+
+made new test, to check if api exists (ran it and failed it, as TDD demands)
--- tests/tests.py
+++ tests/tests.py
@@ -4,5 +4,12 @@ class ApiTestCase(unittest.TestCase):
     def test_one(self):
         self.assertTrue(True)
 
+    def test_api_exists(self):
+        api = '/api/card-scheme/verify/'
+        
+        response = self.app.get(api)
+
+        self.assertEqual(200, response.status_code)
+
 if __name__ == '__main__':
     unittest.main()
