36df60393226c5b55c5882c54ff7b3e876668c68
Rubin <c-rubin@outlook.com>
2024-04-19 22:32:58 +0200

    STEP2 - api successful test - created and passed

--- tests/tests2.py
+++ tests/tests2.py
@@ -8,11 +8,14 @@ api = '/api/card-scheme/stats'
 
 class ApiTestCase(unittest.TestCase):
     def test_api_exists(self):
-        
-        
         response = self.app.get(api+"45717360")
-
         self.assertNotEqual(404, response.status_code)
+
+
+    def test_api_success(self):
+        response = self.app.get(api+"45717360")
+        self.assertEqual(200, response.status_code)
+
 
     def setUp(self):
         self.app = app.test_client(self)
