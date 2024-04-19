e1303c0922943f75813630e913bfd3c46edda79a
Rubin <c-rubin@outlook.com>
2024-04-19 16:51:28 +0200

    valid json test - creation~

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## Step 1
 
-firstly, I created a test class, and just made sure it worked correctly
+firstly, I created a test class, and just made sure it worked correctly <br><br>
 
 made new test, to check if api exists (ran it and failed it, as TDD demands)
 
-successfuly passed the existing test (http code must be different from 404)
+successfuly passed the existing test (http code must be different from 404) <br><br>
 
 made new test to check if api is successfuly responding (http code = 200)
 
-successfuly passed above test
+successfuly passed above test <br><br>
+
+in the next test, I will actually check if the response is a valid json
--- tests/tests.py
+++ tests/tests.py
@@ -5,23 +5,37 @@ import sys
 sys.path.append('../datatellers')
 from api.api import app
 
+import json
+
+api = '/api/card-scheme/verify/'
+
+def isJson(string):
+        try:
+            json.loads(string)
+            return True
+        except ValueError: return False
+
 class ApiTestCase(unittest.TestCase):
     def test_one(self):
         self.assertTrue(True)
 
     def test_api_exists(self):
-        api = '/api/card-scheme/verify/'
+        
         
         response = self.app.get(api)
 
         self.assertNotEqual(404, response.status_code)
 
     def test_api_success(self):
-        api = '/api/card-scheme/verify/'
         
         response = self.app.get(api)
 
         self.assertEqual(200, response.status_code)
+
+    def test_response_json(self):
+        response = self.app.get(api) 
+
+        self.assertTrue(isJson(response.text))
 
     def setUp(self):
         self.app = app.test_client(self)
